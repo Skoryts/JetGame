acd1bcc24cb66817f4cae16628e3da8bc4c0b5f0
Skoryts <skoryts@gmail.com>
2017-07-21 16:27:10 +0300

    Implemented base game object class

---
+++ jet/gameobject.class.js
@@ -0,0 +1,24 @@
+export default class GameObject{
+  constructor(x, y, width, height){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  start(){
+
+  }
+
+  update(){
+    
+  }
+
+  render(){
+    
+  }
+
+  delete(){
+    
+  }
+}
